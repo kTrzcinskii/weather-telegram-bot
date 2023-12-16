84819b8cc540d6055589a705dcb0ff00e25b30fa
kTrzcinskii <kt.trzcinski@gmail.com>
2023-12-16 14:19:06 +0100

    Create enum for `actions`

--- src/actions.ts
+++ src/actions.ts
@@ -1,10 +1,12 @@
-export type TAvailableCommands =
-  | "/here"
-  | "/here_extend"
-  | "/place"
-  | "/place_extend"
-  | "/alerts";
+export enum AvailableCommands {
+  here = "here",
+  here_extend = "here_extend",
+  place = "place",
+  place_extend = "place_extend",
+  alerts = "alerts",
+}
 
+export type TAvailableCommands = keyof typeof AvailableCommands;
 export interface IAction {
   command: TAvailableCommands;
   description: string;
@@ -12,26 +14,26 @@ export interface IAction {
 
 export const actions: IAction[] = [
   {
-    command: "/here",
+    command: AvailableCommands.here,
     description: "I will check basic weather conditions in your area!",
   },
   {
-    command: "/here_extend",
+    command: AvailableCommands.here_extend,
     description:
       "You will be ask for which details you want information on in your area and I will check them!",
   },
   {
-    command: "/place",
+    command: AvailableCommands.place,
     description:
       "You will be asked for a place and I will check basic weather conditions there!",
   },
   {
-    command: "/place_extend",
+    command: AvailableCommands.place_extend,
     description:
       "You will be ask for the place and which details you want information on and I will check them!",
   },
   {
-    command: "/alerts",
+    command: AvailableCommands.alerts,
     description: "I will check for current weather alerts in your area!",
   },
 ];

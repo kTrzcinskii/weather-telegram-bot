40a7d31a1e58b1de53579c4df0faf15ee45a039e
kTrzcinskii <kt.trzcinski@gmail.com>
2023-12-23 16:23:09 +0100

    Make commands listed in help clickable

--- src/handlers/helpHandler.ts
+++ src/handlers/helpHandler.ts
@@ -4,7 +4,7 @@ import { actions } from "../actions";
 export const helpHandler = (ctx: Context) => {
   const message = "Here is a list of available actions:\n";
   const actionsDesc = actions.map(
-    (action) => `${action.command} - ${action.description}`
+    (action) => `/${action.command} - ${action.description}`
   );
   ctx.reply(message + actionsDesc.join("\n"));
 };
